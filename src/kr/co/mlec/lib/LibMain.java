bccf5cbb2f54b4a85083c1b8a308dfd92a48a893
hyunjungchoi <choihj930@gmail.com>
2022-04-14 20:57:58 +0900

    도서조회- DAO, VO, util 커밋

---
+++ src/kr/co/mlec/lib/LibMain.java
@@ -0,0 +1,14 @@
+package kr.co.mlec.lib.dao.src.kr.co.mlec.lib;
+
+import kr.co.mlec.lib.ui.BoardUI;
+
+public class LibMain {
+
+	public static void main(String[] args) {
+		
+		BoardUI lib = new BoardUI();
+		lib.execute();
+		
+	}
+}
+   
---
+++ src/kr/co/mlec/util/connectionFactory.java
@@ -0,0 +1,27 @@
+package kr.co.mlec.lib.dao.src.kr.co.mlec.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class connectionFactory {
+
+	public Connection getConnection() {
+		
+		Connection conn = null;
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			String url = "jdbc:oracle:thin:@localhost";
+			String user = "hr";
+			String password = "hr";
+			conn = DriverManager.getConnection(url, user, password);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+}
+
+
